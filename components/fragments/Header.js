b5e242092d1a0778a56a2c357f0079f23d1d743c
jordanjouin@gmail.com <jordanjouin@gmail.com>
2023-01-24 17:01:59 +0100

    Header updating with connexion/deconnexion

--- components/fragments/Header.js
+++ components/fragments/Header.js
@@ -1,11 +1,32 @@
 import { Row, Col, Layout } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import Link from "next/link";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { async } from "@firebase/util";
+import { firebaseConfig } from "../../config/firebase";
+import React, { useEffect, useState } from "react";
+
+const firebaseApp = initializeApp(firebaseConfig);
 
 function Header() {
 
   const { Header } = Layout;
+  const auth = getAuth(firebaseApp);
+  const db = getFirestore(firebaseApp);
+  const [state, setState] = useState(false);
 
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setState(true)
+      } else {
+        setState(false)
+      }
+    });
+  }, []);
 
   return (
     <Header style={{backgroundColor: "#145369"}}>
@@ -23,10 +44,15 @@ function Header() {
             </a>
           </Link>
         </Col>
-        <Col>
+
+        {state ? (
+                  <Col>
           <Link href="/account">
             <UserOutlined />
           </Link>
+          </Col>
+        ) : (
+        <Col>
           <Link href="/login">
             <a style={{ color: "white", marginLeft: "12px" }}>Se connecter</a>
           </Link>
@@ -34,6 +60,7 @@ function Header() {
             <a style={{ color: "grey", marginLeft: "12px" }}>S'inscrire</a>
           </Link>
         </Col>
+        )}
       </Row>
     </Header>
   );
